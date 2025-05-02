d239849df03cc31fcadd3ae06b2db50e8d27eb2c
KarolFrame <carolina.peseca@gmail.com>
2025-05-02 13:25:16 +0200

    logic traffic light

--- src/app.js
+++ src/app.js
@@ -1,11 +1,46 @@
-import "bootstrap";
-import "./style.css";
+window.onload = trafficLightLoop;
 
+let trafficLightState = "red";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function _delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const _renderLight = (currentState)=> {
+  const redLight = document.querySelector("#red-light");
+  const yellowLight = document.querySelector("#yellow-light");
+  const greenLight = document.querySelector("#green-light");
+  if(currentState == "red"){
+    redLight.style.backgroundColor = "red";
+    yellowLight.style.backgroundColor = "grey";
+    greenLight.style.backgroundColor = "grey";
+  }
+  if(currentState=="yellow"){
+    redLight.style.backgroundColor = "grey";
+    yellowLight.style.backgroundColor = "yellow";
+    greenLight.style.backgroundColor = "grey";
+  }
+  if(currentState=="green"){
+    redLight.style.backgroundColor = "grey";
+    yellowLight.style.backgroundColor = "grey";
+    greenLight.style.backgroundColor = "green";
+  }
+}
+
+const _setStateLight = (state) =>{
+  trafficLightState = state;
+}
+
+async function trafficLightLoop() {
+  while (true) {
+    _setStateLight("red");
+    _renderLight(trafficLightState);
+    await _delay(2000);
+    _setStateLight("yellow");
+    _renderLight(trafficLightState);
+    await _delay(2000);
+    _setStateLight("green");
+    _renderLight(trafficLightState);
+    await _delay(2000);
+  }
+}
